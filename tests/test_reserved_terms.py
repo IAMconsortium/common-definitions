16087b9932f45a4c0106a3520da9e74ec26789c7
Daniel Huppmann <dh@dergelbesalon.at>
2024-04-08 18:07:48 +0200

    Implement a test against using `|Index` or `|Share`

---
+++ tests/test_reserved_terms.py
@@ -0,0 +1,19 @@
+from nomenclature import DataStructureDefinition
+
+
+def test_legacy_variables():
+    # Check that variables use square brackets for operations
+    # https://github.com/IAMconsortium/common-definitions/issues/55
+
+    dsd = DataStructureDefinition("definitions/", dimensions=["variable"])
+
+    error = []
+    for variable, attrs in dsd.variable.items():
+        if reserved_terms := [r for r in ["Index", "Share"] if "|" + r in variable]:
+            error.append(
+                f"Variable '{variable}' -> '[{''.join(reserved_terms)}]'"
+            )
+    if error:
+        raise ValueError(
+            f"Found reserved terms in the following variables:\n{'\n - '.join(error)}"
+        )
