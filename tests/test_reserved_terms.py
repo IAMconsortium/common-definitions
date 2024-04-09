7852a988db76b1983e3ab12602eb5f0fa8dbd356
Daniel Huppmann <dh@dergelbesalon.at>
2024-04-09 10:04:09 +0200

    Simplify test

--- tests/test_reserved_terms.py
+++ tests/test_reserved_terms.py
@@ -8,9 +8,9 @@ def test_variable_ops_as_square_brackets():
     dsd = DataStructureDefinition("definitions/", dimensions=["variable"])
 
     error = []
-    for variable, attrs in dsd.variable.items():
+    for variable in dsd.variable:
         if reserved_terms := [r for r in ["Index", "Share"] if "|" + r in variable]:
-            error.append(f"Variable '{variable}' -> '[{''.join(reserved_terms)}]'")
+            error.append(f"Variable '{variable}' -> '... [{''.join(reserved_terms)}]'")
     if error:
         raise ValueError(
             f"Found reserved terms in the following variables:\n{'\n - '.join(error)}"
