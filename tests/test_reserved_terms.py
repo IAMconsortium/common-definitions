25c8edaf475f7a84b3f419cfe9d47e19a9a2e84f
Daniel Huppmann <dh@dergelbesalon.at>
2024-04-11 10:15:00 +0200

    Add a reference to a nomenclature issue

--- tests/test_reserved_terms.py
+++ tests/test_reserved_terms.py
@@ -3,6 +3,10 @@ from nomenclature import DataStructureDefinition
 RESERVED_TERMS = ["Index", "Share", "Value", "Volume"]
 
 
+# TODO: Move this test to the nomenclature `validate-project` utility
+# see https://github.com/IAMconsortium/nomenclature/issues/341
+
+
 def test_variable_ops_as_square_brackets():
     # Check that variables use square brackets for operations
     # https://github.com/IAMconsortium/common-definitions/issues/55
