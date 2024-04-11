f58273766f7308d3773a669d6a7a841deffba2ae
Daniel Huppmann <dh@dergelbesalon.at>
2024-04-11 11:34:33 +0200

    Guard against methods-terms as sub-category in variable name (#89)

---
+++ tests/test_reserved_terms.py
@@ -0,0 +1,23 @@
+from nomenclature import DataStructureDefinition
+
+RESERVED_TERMS = ["Index", "Share", "Value", "Volume"]
+
+
+# TODO: Move this test to the nomenclature `validate-project` utility
+# see https://github.com/IAMconsortium/nomenclature/issues/341
+
+
+def test_variable_ops_as_square_brackets():
+    # Check that variables use square brackets for operations
+    # https://github.com/IAMconsortium/common-definitions/issues/55
+
+    dsd = DataStructureDefinition("definitions/", dimensions=["variable"])
+
+    error = []
+    for variable in dsd.variable:
+        if reserved_terms := [r for r in RESERVED_TERMS if "|" + r in variable]:
+            error.append(f"Variable '{variable}' -> '... [{''.join(reserved_terms)}]'")
+    if error:
+        raise ValueError(
+            f"Found reserved terms in the following variables:\n{'\n - '.join(error)}"
+        )
